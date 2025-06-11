0b76f6cec1d3162b814d2b6175d16b70023b173d
Harekrishna <adhikaryharekrishna80@gmail.com>
2025-06-11 07:51:57 +0530

    change in server.py using model upload and without model uploading

--- server.py
+++ server.py
@@ -36,3 +36,34 @@ if __name__ == "__main__":
     port = 5001
     print(f"✅ Server running at http://127.0.0.1:{port}/predict")
     app.run(debug=True, port=port)
+
+
+
+# from flask import Flask, request, jsonify
+
+# app = Flask(__name__)
+
+# @app.route("/test", methods=["POST"])
+# def test_input():
+#     data = request.get_json()
+#     print("✅ Received JSON input:")
+#     print(data)
+
+#     # You can also extract values if needed
+#     sepal_length = data.get("sepal_length")
+#     sepal_width = data.get("sepal_width")
+#     petal_length = data.get("petal_length")
+#     petal_width = data.get("petal_width")
+
+#     return jsonify({
+#         "message": "Data received successfully",
+#         "received_data": {
+#             "sepal_length": sepal_length,
+#             "sepal_width": sepal_width,
+#             "petal_length": petal_length,
+#             "petal_width": petal_width
+#         }
+#     })
+
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5001)
